216109dd962cbb183fb36401ac20b4a388c0b77d
lishhPhper <nietzsche_nc@163.com>
2018-06-06 22:36:31 +0800

    展示首页图片页

--- pages/shops/index/index.js
+++ pages/shops/index/index.js
@@ -9,7 +9,27 @@ Page({
     inputVal: "",
     toView: 'red',
     scrollTop: 100,
-    status:1
+    status:2,
+    arry: [
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+
+      { text: "沙发" },
+      { text: "沙发" },
+      { text: "沙发" },
+    ]
   },
   showInput: function () {
     this.setData({
