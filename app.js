9b44300dd57b2f4ad7d6d48e3ea735264fa58632
mengshulin <229557322@qq.com>
2018-06-13 23:42:59 +0800

    modify

--- app.js
+++ app.js
@@ -33,7 +33,51 @@ App({
       }
     })
   },
+  editTabBar: function () {
+      var tabbar = this.globalData.tabbar,
+          currentPages = getCurrentPages(),
+          _this = currentPages[currentPages.length - 1],
+          pagePath = _this.__route__;
+      (pagePath.indexOf('/') != 0) && (pagePath = '/' + pagePath);
+      for (var i in tabbar.list) {
+          tabbar.list[i].selected = false;
+          (tabbar.list[i].pagePath == pagePath) && (tabbar.list[i].selected = true);
+      }
+      _this.setData({
+          tabbar: tabbar
+      });
+  },
   globalData: {
-    userInfo: null
+      userInfo: null,
+      tabbar: {
+          color: "#000000",
+          selectedColor: "#0f87ff",
+          backgroundColor: "#ffffff",
+          borderStyle: "black",
+          list: [
+              {
+                  pagePath: "/pages/tabbar/tabbar",
+                  text: "项目",
+                  iconPath: "/images/item.png",
+                  selectedIconPath: "/images/item_HL.png",
+                  selected: true
+              },
+              {
+                  pagePath: "/pages/address/address",
+                  text: "通讯录",
+                  iconPath: "/images/ts.png",
+                  selectedIconPath: "/images/ts1.png",
+                  selected: false
+              },
+              {
+                  pagePath: "/pages/personal/personal",
+                  text: "文件",
+                  iconPath: "/images/wjj.png",
+                  selectedIconPath: "/images/wjj1.png",
+                  selected: false
+              }
+          ],
+          position: "bottom"
+      }
   }
 })
