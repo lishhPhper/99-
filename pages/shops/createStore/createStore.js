16a537f98263f264f7483c05eee5b41173cc5ad6
shenghui.li <li240538759_shenghui>
2018-06-22 18:37:13 +0800

    表单图片上传

--- pages/shops/createStore/createStore.js
+++ pages/shops/createStore/createStore.js
@@ -82,5 +82,34 @@ Page({
     this.setData({
       isAgree: !!e.detail.value.length
     });
+  },
+  formSubmit: function (e) {
+    var list = e.detail.value;
+    if (agent){
+      // 代理商
+    }else{
+      // 厂家
+    }
+  },
+  gotoShow: function () {
+    var _this = this
+    wx.chooseImage({
+      count: 9, // 最多可以选择的图片张数，默认9
+      sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+      success: function (res) {
+        // success
+        console.log(res)
+        _this.setData({
+          src: res.tempFilePaths
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
   }
 })
