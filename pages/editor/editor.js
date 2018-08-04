24d5c2379e4114467013bc94c6b2215708943858
mengshulin <229557322@qq.com>
2018-08-04 19:35:47 +0800

    修改登录

--- pages/editor/editor.js
+++ pages/editor/editor.js
@@ -1,109 +1,10 @@
-//index.js
 //获取应用实例
 const app = getApp()
 
-var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-    inputShowed: false,
-    inputVal: "",
-    toView: 'red',
-    scrollTop: 100,
     userType: 2,
-    tabbar: {
-        color: "#000000",
-        selectedColor: "#ff6600",
-        backgroundColor: "#ffffff",
-        borderStyle: "black",
-        list: [
-            {
-                pagePath: "/pages/manufactor/index/index",
-                text: "首页",
-                // text: "加微信",
-                iconPath: "../../image/wx_tarbar.png",
-                selectedIconPath: "../../image/wx_tarbarHL.png",
-                selected: true
-            },
-            {
-                pagePath: "/pages/community/index/index",
-                text: "爱我家",
-                // text: "请拨号",
-                iconPath: "../../image/heart.png",
-                selectedIconPath: "../../image/heartHL.png",
-                selected: false
-            },
-            {
-                pagePath: "/pages/personal/personal",
-                text: "",
-                iconPath: "../../image/add.png",
-                selectedIconPath: "../../image/addHL.png",
-                selected: false
-            }
-            ,
-            {
-                pagePath: "/pages/personal/personal",
-                text: "到我店",
-                iconPath: "../../image/map.png",
-                selectedIconPath: "../../image/mapHL.png",
-                selected: false
-            }
-            ,
-            {
-                pagePath: "/pages/info/info",
-                text: "我信息",
-                iconPath: "../../image/myinfo.png",
-                selectedIconPath: "../../image/myinfoHL.png",
-                selected: true
-            }
-        ],
-        position: "bottom"
-    },
   },
-  showInput: function () {
-    this.setData({
-      inputShowed: true
-    });
-  },
-  hideInput: function () {
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
-  inputTyping: function (e) {
-    this.setData({
-      inputVal: e.detail.value
-    });
-  },
-  upper: function (e) {
-    console.log(e)
-  },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
-    }
-  },
-  tapMove: function (e) {
-    this.setData({
-      scrollTop: this.data.scrollTop + 10
-    })
-  }
 });
 
 
